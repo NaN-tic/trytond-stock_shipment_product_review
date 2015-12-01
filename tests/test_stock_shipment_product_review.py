3440d6f69eab1425c89fc9c3672d0c3fee30b788
resteve <resteve@zikzakmedia.com>
2015-12-01 11:26:36 +0100

    Tests

--- tests/test_stock_shipment_product_review.py
+++ tests/test_stock_shipment_product_review.py
@@ -1,31 +1,18 @@
-#!/usr/bin/env python
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_shipment_product_review module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductReviewTestCase(unittest.TestCase):
-    'Test Product Review module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_review')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_review')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class StockShipmentProductReviewTestCase(ModuleTestCase):
+    'Test Stock Shipment Product Review module'
+    module = 'stock_shipment_product_review'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        ProductReviewTestCase))
+        StockShipmentProductReviewTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
